a84a7e795fa18bffa5c60f38ced25906f7bcf89c
jiwon Nam <swm10099@naver.com>
2023-01-30 18:51:00 +0900

    test: useRecoilUser hooks test

--- src/hooks/test/useRecoilUser.test.tsx
+++ src/hooks/test/useRecoilUser.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import useRecoilUser from "hooks/useRecoilUser";
 import { RecoilRoot } from "recoil";
 
@@ -6,10 +6,22 @@ describe("useRecoilUser", () => {
   it("default value of user is null", () => {
     const {
       result: { current },
-    } = renderHook(() => useRecoilUser(), {
-      wrapper: RecoilRoot,
-    });
+    } = render();
 
     expect(current.user).toBeNull();
   });
+
+  it("should set user state", () => {
+    const fakeUser = { id: 1, nickname: "user", email: "email@gmail.com" };
+    const { result } = render();
+
+    act(() => result.current.setUser(fakeUser));
+    expect(result.current.user).toBe(fakeUser);
+  });
+
+  function render() {
+    return renderHook(() => useRecoilUser(), {
+      wrapper: RecoilRoot,
+    });
+  }
 });
