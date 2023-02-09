ea39440dfc7ab033aec20be774cbf37efb53eb16
jiwon Nam <swm10099@naver.com>
2023-02-09 22:43:01 +0900

    style: 변수명 users -> countIcons 변경

--- src/components/commons/UserCount/UserCount.tsx
+++ src/components/commons/UserCount/UserCount.tsx
@@ -3,16 +3,16 @@ import { ReactNode } from "react";
 import styles from "./UserCount.module.css";
 
 export default function UserCount({ number }: { number: number }) {
-  const users: ReactNode[] = [];
+  const countIcons: ReactNode[] = [];
   for (let i = 0; i < number; i += 1) {
     if (i > 5) {
       break;
     }
     if (i <= 4) {
-      users.push(<Person className={styles.person} />);
+      countIcons.push(<Person className={styles.person} />);
     } else if (i === 5) {
-      users.push(<Plus className={styles.plus} />);
+      countIcons.push(<Plus className={styles.plus} />);
     }
   }
-  return <div className={styles.count}>{users}</div>;
+  return <div className={styles.count}>{countIcons}</div>;
 }
