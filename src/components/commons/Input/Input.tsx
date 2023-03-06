95b2311e7682f2d5ffaa55c0eb2060958ee2f84e
jiwon Nam <swm10099@naver.com>
2023-03-07 01:54:43 +0900

    refactor: Input 컴포넌트에 ts 적용

---
+++ src/components/commons/Input/Input.tsx
@@ -0,0 +1,41 @@
+import { forwardRef } from "react";
+import styles from "./Input.module.css";
+
+type InputProps = {
+  type?: string;
+  placeholder?: string;
+  value?: string;
+  maxLength?: number;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyPress?: () => void;
+  textLength?: number;
+  isInvalid?: boolean;
+};
+
+const Input = forwardRef(function Input(
+  { type, placeholder, value, maxLength, onChange, onKeyPress, textLength, isInvalid }: InputProps,
+  inputRef: React.LegacyRef<HTMLInputElement>,
+) {
+  return (
+    <div className={`${styles.text_field} ${isInvalid ? styles.invalid : ""}`}>
+      <input
+        className={styles.input}
+        ref={inputRef}
+        type={type ?? "text"}
+        placeholder={placeholder}
+        value={value}
+        maxLength={maxLength}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+        spellCheck="false"
+      />
+      {textLength !== undefined && (
+        <span className={styles.text_length}>
+          {textLength}/{maxLength}
+        </span>
+      )}
+    </div>
+  );
+});
+
+export default Input;
